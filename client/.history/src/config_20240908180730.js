f2ce6c8f230aa41f85a7f61e6ddbddebe89d1ab2
Aitor Cachero <aitor.cachero@gmail.com>
2024-09-08 18:08:25 +0200

    chage to live

--- client/.history/src/config_20240908180730.js
+++ client/.history/src/config_20240908180730.js
@@ -1,6 +1,6 @@
 const APIUrl_Dev = 'http://localhost:3000';
 const APIUrl_Live = 'https://retropop.up.railway.app';
-const APIUrl = APIUrl_Live;
+const APIUrl = APIUrl_Dev;
 const userLocalStorageKey = 'retroUser';
 
 export { APIUrl, APIUrl_Dev, APIUrl_Live, userLocalStorageKey };
